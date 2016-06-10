a6614fc4eb1bb48255e2a8589d736c6aa604bee7
Joshua Harms <joshua@nozzlegear.com>
2016-06-10 10:16:31 -0500

    Let TypeScript use the TS files for definition lookup, remove typings in favor of ts default behavior.

---
+++ index.ts
@@ -0,0 +1,4 @@
+// Exported definitions should never contain triple-slash references: 
+// https://www.typescriptlang.org/docs/handbook/typings-for-npm-packages.html
+
+export {AutoPropComponent} from "./components/auto-prop-component";
