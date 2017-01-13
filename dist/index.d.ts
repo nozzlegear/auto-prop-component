2bca001419b34b6e33ca959b280b48a6a5f64887
Joshua Harms <joshua@nozzlegear.com>
2017-01-13 15:09:02 -0600

    Do not track built files

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-export declare class AutoPropComponent<P, S> extends React.Component<P, S> {
-    constructor(props: P, context?: any);
-    getAndUpdateState(predicate: (state: S) => S): void;
-    transformState(predicate: (state: S) => void): void;
-    updateStateFromEvent(predicate: (state: S, newValue: any) => void, shouldParse?: boolean): (event: any) => void;
-    mergeState(newState: S, callback?: () => any): void;
-}
